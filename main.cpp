0df3085be65690a118dca96d686f023d8ade6ed6
E.J. Bevenour <ej@ejb1123.tk>
2013-08-20 21:57:52 -0400

    added looping for riping process

--- main.cpp
+++ main.cpp
@@ -5,6 +5,28 @@
 #include <string>
 #include <vector>
 using namespace std;
+string gcommand;
+
+int rip ()
+{
+   system(gcommand.c_str());
+}
+
+int choice ()
+{
+    cout << "ready y/n :";
+    string a;
+    cin >> a;
+    if (a=="y")
+        {
+            rip();
+        }
+      else
+      {
+          cout<<"ok" << endl;
+      }
+      choice();
+}
 
 int main()
 {
@@ -16,16 +38,7 @@ int main()
     cin >> savetodirs;
     string command;
     command = "makemkvcon mkv disc:0 all " + savetodirs;
-    cout << "ready y/n :";
-    string a;
-    cin >> a;
-    if (a=="y")
-        {
-            system(command.c_str());
-        }
-      else
-      {
-          cout << "get ready";
-      }
+    gcommand= command;
+    choice();
     return 0;
 }
